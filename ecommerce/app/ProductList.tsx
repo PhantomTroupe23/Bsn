85f942645f51584b7ea7aedf9220704d1be8bf54
PhantomTroupe23 <phantomlord402@gmail.com>
2025-06-08 09:03:00 +0545

    commit

--- ecommerce/app/ProductList.tsx
+++ ecommerce/app/ProductList.tsx
@@ -5,7 +5,7 @@ export default function ProductList({products}: {products :Product[]}){
 	return(
 		<div className="prodList">
 			{products.map(product =>
-				<Link key ={product.id} href="/product-detail">
+				<Link key ={product.id} href={"/products/" + product.id}>
 					<Image src ={'/'+product.imageUrl} alt="Product image" width={150} height={150}/>
 					<h2>{product.name}</h2>
 					<p>${product.price}</p>
