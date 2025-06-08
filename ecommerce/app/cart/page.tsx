d786b570a9ad464663e1dc6eac40d1b41ad7a7a5
PhantomTroupe23 <phantomlord402@gmail.com>
2025-06-08 09:07:38 +0545

    added not found
    added not found page

--- ecommerce/app/cart/page.tsx
+++ ecommerce/app/cart/page.tsx
@@ -1,3 +1,25 @@
+'use client';
+
+import { useState } from "react";
+import { products } from "../product-data";
+import Link from "next/link";
+
 export default function cartPage(){
-	return <h1> Cart</h1>
+	const [cartIds] = useState(['123','345']);
+	const cartProducts = cartIds.map(id => products.find(p => p.id == id)!);
+	return (
+		<>
+		<div className="cart_card">
+
+
+		{cartProducts.map(product =>
+			<Link key={product.id} href={"/products/"+product.id}>
+				<h3>{product.name}</h3>	
+				<p>{product.price}</p>
+			</Link>
+		)}
+		</div>
+		<h1> Cart</h1>
+		</>
+	); 
 }
--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -1,3 +1,19 @@
-export default function ProductsDetailsPage(){
-	return <h1> Products Details</h1>
+import NotFoundPage from "@/app/not-found";
+import { products } from "@/app/product-data";
+
+export default function ProductsDetailsPage({ params }: { params: { id: string } }) {
+  const product = products.find(p => p.id === params.id);
+
+  if (!product) {
+    return <NotFoundPage/>;
+  }
+
+  return (
+  <>
+  <h1>{product!.name}</h1>
+  <p className="prod-price">{product!.price}</p>
+  <h3>Description</h3>
+  <p>{product.description}</p>
+  </>
+  );
 }
